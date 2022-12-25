989605ead521e0db01598a7ec91876a9223a1b79
Greg <mauve.simian@gmail.com>
2022-12-26 02:56:22 +1000

    Add placeholder EndGame mode

--- src/components/EndGame/EndGame.js
+++ src/components/EndGame/EndGame.js
@@ -3,8 +3,10 @@ import styles from './styles.module.css';
 export const EndGame = ({onRestart}) => {
     return (
         <div className={styles.main}>
-            Game over!
-            <button onClick={onRestart}>Return to Start</button>
+            <h1>PLACEHOLDER has won!</h1>
+            <button className={styles.startButton} onClick={onRestart}>
+                Play Again
+            </button>
         </div>
     );
 }
