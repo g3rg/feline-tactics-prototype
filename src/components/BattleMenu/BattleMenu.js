0dfdfa7ba99286f25d5104379c19c3b30bdda5f4
Greg <mauve.simian@gmail.com>
2022-12-28 06:27:20 +1000

    Add special button, rename magic, but still does spell for now

--- src/components/BattleMenu/BattleMenu.js
+++ src/components/BattleMenu/BattleMenu.js
@@ -1,15 +1,18 @@
 import styles from './styles.module.css';
 
-export const BattleMenu = ({ onAttack, onMagic, onHeal }) => (
-  <div className={styles.main}>
-    <div onClick={onAttack} className={styles.option}>
-      Attack
+export const BattleMenu = ({onAttack, onMagic, onHeal}) => (
+    <div className={styles.main}>
+        <div onClick={onAttack} className={styles.option}>
+            Attack
+        </div>
+        <div onClick={onMagic} className={styles.option}>
+            Defend
+        </div>
+        <div onClick={onHeal} className={styles.option}>
+            Heal
+        </div>
+        <div className={styles.option}>
+            Special
+        </div>
     </div>
-    <div onClick={onMagic} className={styles.option}>
-      Magic
-    </div>
-    <div onClick={onHeal} className={styles.option}>
-      Heal
-    </div>
-  </div>
 );
