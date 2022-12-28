44dcb255c3b5c179712ee0001f0a8770cebbc735
Greg <mauve.simian@gmail.com>
2022-12-29 00:14:45 +1000

    Start styling character select screen

--- src/components/CharacterSelect/CharacterSelect.js
+++ src/components/CharacterSelect/CharacterSelect.js
@@ -1,8 +1,12 @@
+import styles from './styles.module.css';
+
 export const CharacterSelect = ({onClick}) => {
     return (
-        <div>
-            Select Your Character!
-            <button onClick={onClick}>Start!</button>
+        <div className={styles.main}>
+            <div>Select Your Character!</div>
+            <div>
+                <button className={styles.startButton} onClick={onClick}>Start!</button>
+            </div>
         </div>
     )
 }
