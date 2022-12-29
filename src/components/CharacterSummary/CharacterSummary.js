e93d932e3345de55de90b9719cc38d901dfb35e7
Greg <mauve.simian@gmail.com>
2022-12-30 02:20:38 +1000

    Further character selection formatting

--- src/components/CharacterSummary/CharacterSummary.js
+++ src/components/CharacterSummary/CharacterSummary.js
@@ -3,17 +3,34 @@ import styles from './styles.module.css';
 
 export const CharacterSummary = ({selectedCharDetails}) => {
     return (
-        <>
+        <div className={styles.characterView}>
+            <div className={styles.info}>
+                <div className={styles.playerOneSprite}>
+                    <img
+                        alt={selectedCharDetails.name}
+                        src={selectedCharDetails.img}
+                    />
+                </div>
+            </div>
             <div className={styles.info}>
                 <div className={styles.name}>{selectedCharDetails.name}</div>
             </div>
-
             <div className={styles.health}>
-                <Bar alt label="HP" value={selectedCharDetails.maxHealth} maxValue={200}/>
+                <Bar alt label="HP" value={selectedCharDetails.maxHealth} maxValue={100}/>
+            </div>
+            <div className={styles.atk}>
+                <Bar label="ATK" value={selectedCharDetails.attack} maxValue={100}/>
+            </div>
+            <div className={styles.def}>
+                <Bar label="DEF" value={selectedCharDetails.defense} maxValue={100}/>
             </div>
             <div className={styles.power}>
-                <Bar label="POW" value={selectedCharDetails.maxPower} maxValue={200}/>
+                <Bar label="POW" value={selectedCharDetails.maxPower} maxValue={100}/>
+            </div>
+            <div className={styles.heal}>
+                <Bar label="HEAL" value={selectedCharDetails.healing} maxValue={100}/>
             </div>
 
-        </>)
+        </div>
+    )
 }
