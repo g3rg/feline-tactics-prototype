55d4b46c0ca17b92c6c423885b1d1033ed1c613c
Greg <mauve.simian@gmail.com>
2022-12-31 16:54:00 +1000

    Make power bar change colour when reach full power

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -1,14 +1,21 @@
 import styles from './styles.module.css';
 
 export const Bar = ({value, maxValue, label, alt}) => {
-    const className = alt ? styles.valueAlt : styles.value
+    const pcVal = (value / maxValue) * 100
+
+    let classType = alt ? 'valueAlt' : 'value'
+    if (pcVal === 100) {
+        classType += 'Full'
+    }
+    const className = styles[classType]
+    
     return (
         <div className={styles.main}>
             <div className={styles.label}>{label}</div>
             <div className={styles.max}>
                 <div
                     className={className}
-                    style={{width: `${(value / maxValue) * 100}%`}}
+                    style={{width: `${pcVal}%`}}
                 ></div>
             </div>
         </div>
